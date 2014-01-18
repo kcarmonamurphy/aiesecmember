0a899e420123b122b9e0a2ead75e9f4aca5f5db9
Kevin Carmona-Murphy <kevcom@live.ca>
2014-01-17 21:52:13 -0500

    Working again. Saves all data properly from first and second pages with most of the validation up to date

--- platform.rb
+++ platform.rb
@@ -11,7 +11,7 @@ require "sinatra/reloader" if development?
 set :server, 'webrick'
 
 DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db.db")
-DataMapper::Model.raise_on_save_failure = true
+DataMapper::Model.raise_on_save_failure = false
 DataMapper::Property.accept_options(:field_title)
 
 class Candidate
@@ -19,89 +19,59 @@ class Candidate
 
 	property :id, Serial, :required => true
 
-	property :full_name, String, :field_title => "Full Name",
-	:messages => {
-		:presence 	=> "Please provide your full name"
-	}
-	#validates_presence_of :full_name, :when => [ :first_page ]
+	property :full_name, String, :field_title => "Full Name"
+	validates_presence_of :full_name, :message => "Please provide your full name", :when => [ :first_page ]
 
-	property :prog_level, String, :field_title => "Program & Level",
-    :messages => {
-      	:presence 	 => "Please provide your program and level",
-      	:format   	 => "Invalid email address"
-    }
-    #validates_presence_of :prog_level, :when => [ :first_page ]
+	property :prog_level, String, :field_title => "Program & Level"
+    validates_presence_of :prog_level, :message => "Please provide your program and level", :when => [ :first_page ]
 
-	property :birthdate, String, :field_title => "Birthdate",
-	:messages => {
-		:presence 	=> "Please provide your birthdate"
-	}
-	#validates_presence_of :birthdate, :when => [ :first_page ]
+	property :birthdate, String, :field_title => "Birthdate"
+	validates_presence_of :birthdate, :message => "Please provide your birthdate", :when => [ :first_page ]
 
-	property :stud_num, String, :field_title => "Student Number", :length => 7,
-	:messages => {
-		:presence 	=> "Please provide your student number",
-		:length		=> "Must be 7 digits"
-	}
-	#validates_presence_of :stud_num, :when => [ :first_page ]
+	property :stud_num, String, :field_title => "Student Number"
+	validates_presence_of :stud_num, :message => "Please provide your student number", :when => [ :first_page ]
+	validates_length_of :stud_num, :within => (7..7), :message => "Make sure your student number is 7 characters", :when => [ :first_page ]
 
-	property :mac_email, String, :field_title => "McMaster Email", :format => :email_address,
-	:messages => {
-		:presence 	 => "Please provide your mac email address",
-      	:format   	 => "Invalid email address"
-	}
-	#validates_presence_of :mac_email, :when => [ :first_page ]
+	property :mac_email, String, :field_title => "McMaster Email", :format => :email_address
+	validates_presence_of :mac_email, :message => "Please provide your mac email address", :when => [ :first_page ]
+	validates_format_of :mac_email, :as => :email_address, :message => "Invalid format", :when => [ :first_page ]
 
-	property :alt_email, String, :field_title => "Alternative Email", :format => :email_address,
-	:messages => {
-		:presence 	 => "Please provide an alternative email address",
-      	:format   	 => "Invalid email address"
-	}
-	#validates_presence_of :alt_email, :when => [ :first_page ]
+	property :alt_email, String, :field_title => "Alternative Email", :format => :email_address
+	validates_presence_of :alt_email, :message => "Please provide an alternative email address", :when => [ :first_page ]
+	validates_format_of :alt_email, :as => :email_address, :message => "Invalid format", :when => [ :first_page ]
 
-	property :phone_num, String, :field_title => "Phone Number",
-	:messages => {
-		:presence 	=> "Please provide your phone number"
-	}
-	#validates_presence_of :phone_num, :when => [ :first_page ]
+	property :phone_num, String, :field_title => "Phone Number"
+	validates_presence_of :phone_num, :message => "Please provide your phone number", :when => [ :first_page ]
 
-	property :perm_address, String, :field_title => "Permanent Address",
-	:messages => {
-		:presence 	=> "Please provide your permanent address"
-	}
-	#validates_presence_of :perm_address, :when => [ :first_page ]
+	property :perm_address, String, :field_title => "Permanent Address"
+	validates_presence_of :perm_address, :message => "Please provide your permanent address", :when => [ :first_page ]
 
-	property :curr_address, String, :field_title => "Current Address",
-	:messages => {
-		:presence 	=> "Please provide your current address"
-	}
-	#validates_presence_of :curr_address, :when => [ :first_page ]
+	property :curr_address, String, :field_title => "Current Address"
+	validates_presence_of :curr_address, :message => "Please provide your current address", :when => [ :first_page ]
 
 	property :gender, String, :field_title => "Gender"
-	#validates_presence_of :gender, :when => [ :first_page ]
-
-
+	validates_presence_of :gender, :when => [ :first_page ]
 
 	property :q1, String, :field_title => "Which position(s) interest you the most. (Please refer to our Job Catalogue for details)"
-	#validates_presence_of :q1, :when => [ :second_page ]
+	validates_presence_of :q1, :when => [ :second_page ]
 
 	property :q2, String, :field_title => "List your top 3 relevant professional experiences in chronological order. Include dates, employer, and what experiences you gained."
-	#validates_presence_of :q2, :when => [ :second_page ]
+	validates_presence_of :q2, :when => [ :second_page ]
 
 	property :q3, String, :field_title => "What is your main goal and motivation to become part of AIESEC?"
-	#validates_presence_of :q3, :when => [ :second_page ]
+	validates_presence_of :q3, :when => [ :second_page ]
 
 	property :q4, String, :field_title => "What skills would you like to develop by being involved with AIESEC?"
-	#validates_presence_of :q4, :when => [ :second_page ]
+	validates_presence_of :q4, :when => [ :second_page ]
 
 	property :q5, String, :field_title => "What does leadership mean to you? Name 3 key skills or traits you possess. (eg. Strategic thinking, patience, methodical)"
-	#validates_presence_of :q5, :when => [ :second_page ]
+	validates_presence_of :q5, :when => [ :second_page ]
 
 	property :q6, String, :field_title => "What type of person do you enjoy working with?"
-	#validates_presence_of :q6, :when => [ :second_page ]
+	validates_presence_of :q6, :when => [ :second_page ]
 
 	property :q7, String, :field_title => "Describe a situation in which you had a lot of things to do at the same time and how you managed fulfill all responsibilities on time."
-	#validates_presence_of :q7, :when => [ :second_page ]
+	validates_presence_of :q7, :when => [ :second_page ]
 
 	property :newfield, String, :field_title => "new field"
 
@@ -152,7 +122,7 @@ class Platform < Sinatra::Base
 		c.hex = generate_token(:hex, 4)
 
 		#save record
-   		if c.save
+   		if c.save(:first_page)
      		redirect "/#{c.hex}/pagetwo"
    		else
    			#error on save, show error messages
@@ -178,27 +148,21 @@ class Platform < Sinatra::Base
 	post '/:ca/pagetwo/process' do |ca|
 		c = Candidate.first(:hex => ca)
 
-		model_properties(Candidate, :q1, :q7).each do |field|
+		model_properties(Candidate, :q1, :q7).each do |field, name|
 			c[field] = session[field] = params[field]
-			puts session[field].inspect
 		end
 
-		c.save
-
-		redirect "/#{c.hex}/pagethree"
-
-		# #save record
-  #  		if c.save(:first_page)
-  #    		redirect "/#{c.hex}/pagethree"
-  #  		else
-  #  			#error on save, show error messages
-  #  			c.attributes.each do |a|
-  #  				field = a.first
-  #  				puts field.inspect
-		# 		flash[field] = c.errors.on(field) ? c.errors.on(field).first : nil
-		# 	end
-  #    		redirect "/#{c.hex}/pagetwo"
-  # 		end
+		#save record
+   		if c.save(:second_page)
+     		redirect "/#{c.hex}/pagethree"
+   		else
+   			#error on save, show error messages
+   			c.attributes.each do |a|
+   				field = a.first
+				flash[field] = c.errors.on(field) ? c.errors.on(field).first : nil
+			end
+     		redirect "/#{c.hex}/pagetwo"
+  		end
 
 	end
 
@@ -215,7 +179,7 @@ class Platform < Sinatra::Base
 	post '/:ca/pagethree/process' do |ca|
 		c = Candidate.first(:hex => ca)
 
-		model_properties(Candidate, :newfield, :newfield).each do |field|
+		model_properties(Candidate, :newfield, :newfield).each do |field, name|
 			c[field] = params[field]
 		end
 
